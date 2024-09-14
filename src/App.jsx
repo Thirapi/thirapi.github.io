f0d1ad5ccbf58f0c83df6b7c7cd64601dafc8bec
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-09-14 09:32:50 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 function App() {
+  const openInNewTab = () => {
+    window.open('https://thirapi.vercel.app/', '_blank', 'noopener,noreferrer');
+  };
+
+  const commingSoon = () => {
+    alert("under construction")
+  }
   return (
     <>
-      <h1>thirapi<span>/archive</span></h1>
+      <main>
+      <span className='logo'>Thirapi</span>
+      <ul className='menu'>
+      <li className='menu--items' onClick={openInNewTab}>blog</li>
+      <li className='menu--items' onClick={commingSoon}>archive</li>
+      </ul>
+      </main>
     </>
   )
 }
